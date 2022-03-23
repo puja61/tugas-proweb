f9a6ead39cdd7ab3c6897dd771acb9c874621fc2
puja <putrinjani668@gmail.com>
2022-03-23 15:51:50 +0800

    tugas proweb

---
+++ latiaaan.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int x = 7;
+	int y = 8;
+	bool hasil;
+	cout<<"masukkan x: "<<x<<endl;
+	cout<<"masukkan y: "<<y<<endl;
+	cout<<"AND"<<endl;
+	hasil=(x==7) AND (y==6);
+	cout<<"hasil : "<<hasil<<endl;
+	return 0;
+}
